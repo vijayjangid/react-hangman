d6a9e4d2be68e7010053406f3fbcc75938f456d8
Vijay Jangid <vijskt@gmail.com>
2021-05-11 16:16:54 +0530

    removed result area while game in progress

--- src/components/result/index.js
+++ src/components/result/index.js
@@ -4,12 +4,15 @@ import { GAME_RESULT } from "../../constants";
 export default function Result({ result, guessword }) {
   let resultClass = "",
     resultText = "😊";
+
   if (result === GAME_RESULT.WON) {
     resultClass = "success";
     resultText = "🥳 Congratulations!!! You WON.";
   } else if (result === GAME_RESULT.LOST) {
     resultClass = "error";
     resultText = `😞 Oops!!! You Lost. The word was '${guessword}'`;
+  } else {
+    return null;
   }
 
   return (
