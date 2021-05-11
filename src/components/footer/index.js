175d390a6f5766b0d106bfcc5f7ff4daf1c10b71
Vijay Jangid <vijskt@gmail.com>
2021-05-11 07:36:01 +0530

    added github link in footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,17 +2,29 @@ import "./style.css";
 export default function Footer() {
   return (
     <div className="footer skeuo disabled">
-      Logo made by
-      <a
-        href="https://www.flaticon.com/authors/smalllikeart"
-        title="smalllikeart"
-      >
-        smalllikeart
-      </a>
-      from
-      <a href="https://www.flaticon.com/" title="Flaticon">
-        www.flaticon.com
-      </a>
+      <div>
+        Github:{" "}
+        <a
+          href="https://github.com/vijayjangid/react-hangman"
+          title="Github React-Hangman"
+        >
+          @vijayjangid/react-hangman
+        </a>
+      </div>
+      <div className="divider" />
+      <div>
+        Logo made by
+        <a
+          href="https://www.flaticon.com/authors/smalllikeart"
+          title="smalllikeart"
+        >
+          smalllikeart
+        </a>
+        from
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </div>
     </div>
   );
 }
